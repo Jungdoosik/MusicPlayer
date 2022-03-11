2c5bd81714bb1b0be04729ce297422a6252a7168
Doosik Jung <entlrsla@gmail.com>
2022-03-11 18:07:25 +0900

    너무 어려움(시간나면 다시해봐야겠음) - 완성

--- js/main.js
+++ js/main.js
@@ -1,12 +1,91 @@
 const frame = document.querySelector("section");
 const lists = frame.querySelectorAll("article");
 
+const prev = document.querySelector(".btnPrev");
+const next = document.querySelector(".btnNext");
+
+const audio = frame.querySelectorAll("audio");
+
+
 const deg = 45;
 const len = lists.length-1;
 
 let i = 0;
+let num = 0;
+
+let active = 0;
 
 for(let el of lists){
+  let pic = el.querySelector(".pic");
+
+  let play = el.querySelector(".play");
+  let pause = el.querySelector(".pause");
+  let load = el.querySelector(".load");
+
   el.style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
+  pic.style.backgroundImage = `url(../img/member${i+1}.jpg)`;
   i++;
+
+  play.addEventListener("click", e => {
+    let isActive = e.currentTarget.closest("article").classList.contains("on");
+
+    if(isActive){
+      e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+      e.currentTarget.closest("article").querySelector("audio").play();
+    }
+  });
+
+  pause.addEventListener("click", e => {
+    let isActive = e.currentTarget.closest("article").classList.contains("on");
+
+    if(isActive){
+      e.currentTarget.closest("article").querySelector(".pic").classList.remove("on");
+      e.currentTarget.closest("article").querySelector("audio").pause();
+    }
+    
+  });
+
+  load.addEventListener("click", e => { /*다시듣기*/
+    let isActive = e.currentTarget.closest("article").classList.contains("on");
+
+    if(isActive){
+      e.currentTarget.closest("article").querySelector(".pic").classList.add("on");
+      e.currentTarget.closest("article").querySelector("audio").load();
+      e.currentTarget.closest("article").querySelector("audio").play();
+    }
+  
+  })
+}
+
+prev.addEventListener("click", () => {
+  initMusic();
+  num++;
+  frame.style.transform = `rotate(${deg * num}deg)`;
+
+  active == 0 ? active = len : active--;
+  activation(active, lists);
+});
+
+next.addEventListener("click", () => {
+  initMusic();
+  num--;
+  frame.style.transform = `rotate(${deg * num}deg)`;
+
+  active == len ? active = 0 : active++;
+  activation(active, lists);
+})
+
+function activation(index,lists){
+  for( let el of lists){
+    el.classList.remove("on");
+  }
+  lists[index].classList.add("on");
+}
+
+function initMusic(){
+  for(let el of audio){
+    el.pause();
+    el.load();
+    el.parentElement.previousElementSibling.classList.remove("on");
+  }
 }
